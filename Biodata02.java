5c25917ee0ff2a439c4afc516dd34f1dc05e4494
ArdhanFah <dikriardhn82@gmail.com>
2024-09-05 16:00:30 +0700

    ini data ardhan

---
+++ Biodata02.java
@@ -0,0 +1,16 @@
+public class Biodata02 {
+        public static void main(String[] args) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi-POLINEMA --");
+            System.out.println("---------------------------------------------------------");
+            System.out.println("Nama      : Ardhan Dikri Achmad Fahrudin");
+            System.out.println("NIM       : 244107020012");
+            System.out.println("Kelas     : TI-1F");
+            System.out.println("No Absen  : 02");
+            System.out.println("Prodi     : D-IV Teknik Informatika");
+            System.out.println("Alamat    : Kecamatan Gedangan, Kabupaten Malang");
+            System.out.println("No HP     : 0838-3067-2252");
+            System.out.print  ("Hobi      : Jogging");
+        }
+
+}
